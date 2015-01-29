03c01d6c777913808b1af222bf835d2fcbf02a1d
Nicholas Kirsten <nicholas.kirsten@ikhasi.co.za>
2015-01-29 12:10:22 +0200

    post notification on category select

--- CatBarTVC.m
+++ CatBarTVC.m
@@ -21,18 +21,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-   
+    
     categories =[NSArray arrayWithObjects:@"Top Stories",@"South Africa",@"World",@"Sport",@"Business",nil];
     _tableView = [[UITableView alloc] initWithFrame: CGRectMake(0,0, 0,0)
-                                             style:UITableViewStylePlain];
+                                              style:UITableViewStylePlain];
     [_tableView setDataSource:self];
     [_tableView setDelegate:self];
     
     _tableView.autoresizesSubviews=NO;
     _tableView.separatorColor = [UIColor greenColor];
-     self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
-
-
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+    
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
@@ -69,65 +69,78 @@
     
     cell.nameLabel.text = [categories objectAtIndex:indexPath.row];
     
-
+    
     return cell;
     
 }
+
+
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath*)indexPath
 {
-
+    
     return 70;
 }
 
-/*
-- (void)tableView: (UITableView *)tableView didSelectRowAtIndexPath: (NSIndexPath *)indexPath
-{
-    //ArticlesTVC *a = [[ArticlesTVC class] performSelector:@selector(viewDidLoad)];
+//NICK: added method that fires when a tableview cell is tapped
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    //Fetch the category that was tapped
+    NSString *categoryString = [categories objectAtIndex:indexPath.row];
+    
+    //Post a global notification that can be picked up by any other view in our app. Normally we store the string constants (ie. "kNotificationKey_categorySelected") in a globally accessible file but we'll get to that at a later stage.
+    //This line basically "broadcasts" a message saying a category was selected, and it sends the category string with the message
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"kNotificationKey_categorySelected" object:categoryString];
 }
-
-
-
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
 /*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
+ - (void)tableView: (UITableView *)tableView didSelectRowAtIndexPath: (NSIndexPath *)indexPath
+ {
+ //ArticlesTVC *a = [[ArticlesTVC class] performSelector:@selector(viewDidLoad)];
+ }
+ 
+ 
+ 
+ // Override to support conditional editing of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
 
 /*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
+ // Override to support editing the table view.
+ - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+ if (editingStyle == UITableViewCellEditingStyleDelete) {
+ // Delete the row from the data source
+ [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+ } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+ // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+ }
+ }
+ */
 
 /*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
+ // Override to support rearranging the table view.
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
 
 /*
-#pragma mark - Navigation
+ // Override to support conditional rearranging of the table view.
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
